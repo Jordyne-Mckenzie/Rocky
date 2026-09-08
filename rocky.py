169343b3f44e1c0678a2aabbdecfc7c80073e711
Jordyne-Mckenzie <jordyne.mckenzie218@gmail.com>
2026-09-09 11:59:27 +1200

    Update rocky.py
    updated code to decide move based on opps past move history

--- rocky.py
+++ rocky.py
@@ -1,20 +1,24 @@
-# history = [
-#     {
-#         "round": 1,
-#         "player": "Rock",
-#         "opponent": "Paper",
-#         "round_winner": "Opp"
-#     },
-#     {
-#         "round": 2,
-#         "player": "Scissors",
-#         "opponent": "Paper",
-#         "round_winner": "Player"
-#     }
-# ]
+history = [
+     {
+        "round": 1,
+       "player": "rock",
+         "opponent": "paper",
+         "round_winner": "Opp"
+     },
+     {
+         "round": 2,
+         "player": "scissors",
+         "opponent": "paper",
+         "round_winner": "Player"
+     }
+ ]
 import random
-def rocky():
-    return(random.choice(["rock", "paper", "scissors"]))
-
-
-print(rocky())
+def rocky(round_history):
+    if not round_history:
+       return(random.choice(["rock", "paper", "scissors"]))
+    opponent_move = [past_history["opponent"] for past_history in round_history]
+    counts = {"rock":opponent_move.count("rock"),"paper":opponent_move.count("paper"),"scissors":opponent_move.count("scissors")}
+    used_most_move = max(counts, key=counts.get)
+    counter = {"paper":"scissors","rock":"paper","scissors":"rock"}
+    return counter[used_most_move]
+print(rocky(history))
