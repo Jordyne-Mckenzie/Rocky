7f0bb8045f0ebeb81c455ddda181dbe760b9c6ad
Jordyne-Mckenzie <jordyne.mckenzie218@gmail.com>
2026-09-09 12:00:26 +1200

    Create History_test.py

---
+++ History_test.py
@@ -0,0 +1,15 @@
+history = [
+    {
+        "round": 1,
+        "player": "Rock",
+        "opponent": "Paper",
+        "round_winner": "Opp"
+    },
+    {
+        "round": 2,
+        "player": "Scissors",
+        "opponent": "Paper",
+        "round_winner": "Player"
+    }
+]
+print(history[-1]["opponent"])
